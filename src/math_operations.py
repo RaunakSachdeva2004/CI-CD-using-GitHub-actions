a0ee1805cbaf7d6aab5f53ad9ed7299ed442b2a3
Raunak Sachdeva <raunakdachdeva2004@gmail.com>
2026-06-19 19:38:41 +0530

    unit testcases  updated

---
+++ src/math_operations.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    """
+    Simply just adds two numbers a and b given in the arguments
+    """
+    
+    return a+b
+
+def subtract(a,b):
+    """
+    Simply just subtracts two numbers a and b given in the arguments
+    """
+    
+    return a-b
